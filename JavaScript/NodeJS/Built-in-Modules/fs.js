195f3f09796534f9c3ad419dedb47de2b56ceb52
vighnesh153 <vighnesh.raut13@gmail.com>
2020-01-12 18:47:03 +0530

    add asynchronous way to write to files

--- JavaScript/NodeJS/Built-in-Modules/fs.js
+++ JavaScript/NodeJS/Built-in-Modules/fs.js
@@ -1,4 +1,14 @@
 const fs = require('fs');
 
-fs.writeFileSync('FILE_NAME', 'FILE_CONTENT');
 // writes to file synchronously
+fs.writeFileSync('FILE_NAME', 'FILE_CONTENT');
+
+
+// Asynchronous write to file
+fs.writeFile('FILE_NAME', 'FILE_CONTENT', err => {
+  if (err) {
+    console.log('Error writing to file.');
+  } else {
+    console.log('Write to file was successful.');
+  }
+});
