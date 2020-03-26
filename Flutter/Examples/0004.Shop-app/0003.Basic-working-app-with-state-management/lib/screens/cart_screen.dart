7613cd20f39ea3a87d5c289a22af07088491f5b0
vighnesh153 <vighnesh.raut13@gmail.com>
2020-03-26 09:33:34 +0530

    add the basic working app with state management

---
+++ Flutter/Examples/0004.Shop-app/0003.Basic-working-app-with-state-management/lib/screens/cart_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart' show Cart;
+import '../providers/orders.dart';
+import '../widgets/cart_item.dart';
+
+class CartScreen extends StatelessWidget {
+  static const routeName = '/cart';
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Cart'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                children: <Widget>[
+                  Text('Total', style: TextStyle(fontSize: 20)),
+
+                  // Takes up available space and
+                  // pushes the widgets after it to
+                  // the right
+                  Spacer(),
+
+                  Chip(
+                    label: Text(
+                      '\$${cart.totalAmount.toStringAsFixed(2)}',
+                      style: TextStyle(
+                          color:
+                              Theme.of(context).primaryTextTheme.title.color),
+                    ),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  FlatButton(
+                    child: Text('ORDER NOW'),
+                    onPressed: () {
+                      Provider.of<Orders>(
+                        context,
+                        listen: false,
+                      ).addOrder(
+                        cart.items.values.toList(),
+                        cart.totalAmount,
+                      );
+                      cart.clear();
+                    },
+                    textColor: Theme.of(context).primaryColor,
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (ctx, index) => CartItem(
+                id: cart.items.values.toList()[index].id,
+                productId: cart.items.keys.toList()[index],
+                title: cart.items.values.toList()[index].title,
+                quantity: cart.items.values.toList()[index].quantity,
+                price: cart.items.values.toList()[index].price,
+              ),
+              itemCount: cart.items.length,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ Flutter/Examples/0004.Shop-app/0003.Basic-working-app-with-state-management/lib/widgets/product_item.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+import '../providers/product.dart';
+import '../providers/cart.dart';
+
+import '../screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Don't listen to updates because title, imageUrl
+    // won't change. Only the isFavourite boolean will
+    // change.
+    final product = Provider.of<Product>(context, listen: false);
+    final cart = Provider.of<Cart>(context, listen: false);
+
+    // Now here, only the widget which is wrapped in
+    // 'Consumer' will re-build on changes, in this case,
+    // the favouriteIcon
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: product.id,
+            );
+          },
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: Consumer<Product>(
+            // If we have a widget that doesn't need to
+            // be re-built in the consumer's builder,
+            // then we pass it as a value to 'child'
+            // property besides 'builder', and then
+            // in the builder's value, the function's
+            // 3rd parameter will be that child property
+            // which can be used to build the widget
+            builder: (ctx, product, child) {
+              return IconButton(
+                icon: Icon(product.isFavourite
+                    ? Icons.favorite
+                    : Icons.favorite_border),
+                onPressed: () {
+                  product.toggleFavouriteStatus();
+                },
+                color: Theme.of(context).accentColor,
+              );
+            },
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              cart.addItem(
+                product.id,
+                product.price,
+                product.title,
+              );
+            },
+            color: Theme.of(context).accentColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
