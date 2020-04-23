b7fd1962e3bba978e15d3ff983b6fb41ba44a4d6
vighnesh153 <vighnesh.raut13@gmail.com>
2020-04-23 19:35:04 +0530

    add about streaming data

--- JavaScript/NodeJS/Built-in-Modules/fs.js
+++ JavaScript/NodeJS/Built-in-Modules/fs.js
@@ -12,3 +12,17 @@ fs.writeFile('FILE_NAME', 'FILE_CONTENT', err => {
     console.log('Write to file was successful.');
   }
 });
+
+// Similarlly, we have the readFile and readFileSync
+
+
+
+// Streaming files: in express
+const file = fs.createReadStream('path/to/file');
+res.setHeader('Content-Type', 'application/pdf');
+res.setHeader(
+    'Content-Disposition',
+    `attachment; filename="${name_of_file}"`
+);
+// 'attachment' means it will asked to be downloaded
+// 'inline', which is default, will be serverd by browser if it can else asked to be downloaded.
