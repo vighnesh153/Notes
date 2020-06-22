b53c4cb214db8a4e2bb72f3ffca5fef0940c2fd8
vighnesh153 <vighnesh.raut13@gmail.com>
2020-06-22 10:41:54 +0530

    add basics of slices

---
+++ Golang/0010.slices.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	s := make([]string, 3)
+	fmt.Println("emp:", s)
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
+
+	// fmt.Println("len:", len(s))
+
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd:", s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy:", c)
+
+	for i, ch := range s {
+		println(i, ch)
+	}
+
+	l := s[2:5]
+	fmt.Println("sl1:", l)
+
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	l = s[2:]
+	fmt.Println("sl3:", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	twoD := make([][]int, 3)
+	fmt.Println("2d: ", twoD)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
+
+	size := 10			// actual size or number of elements in arr
+	capacity := 16		// initial capacity. when len(arr) becomes 16
+						// and new element is added, the capacity is doubled.
+						// Table doubling occurs
+	arr := make([]int, size, capacity)
+	println(arr)
+
+}
